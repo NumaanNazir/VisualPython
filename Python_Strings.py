5dfa44770488683c9a02dc2754a86d993a0d26af
NumaanNazir <nq.nqanoon@gmail.com>
2024-01-25 14:00:24 +0530

    Added Strings

---
+++ Python_Strings.py
@@ -0,0 +1,16 @@
+# SINGLE QUOTE (' ')
+
+print('Hello Github users')
+
+#DOUBLE QUOTES (" ") 
+#We can use apostrophe withn double quotes.
+
+print("Hello Github user's")
+
+
+#TRIPLE QUOTES (''' ''') 
+# Spanning strings over multiple lines can be done using python’s triple quotes with apostrophe
+
+print('''Hello
+      Github
+      User's''')
