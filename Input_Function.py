80176f5325b0639202132bcc4f84bd37793c7f40
Numaan Nazir <152082026+NumaanNazir@users.noreply.github.com>
2024-01-27 14:18:27 +0530

    Add files via upload

---
+++ Input_Function.py
@@ -0,0 +1,7 @@
+#INPUT FUNCTIONS
+x=input("Hello There! Whats your Profession ?  ")
+print("I am a" , x )
+
+#INPUT FUNCTION WITH INT
+y=int(input("For how much did you sell this house for?"))
+print("I sold this house approx for" , y,"$")
